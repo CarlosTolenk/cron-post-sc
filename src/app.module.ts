aca1c9faa56393b92830f8d3abf65510a11745dd
CarlosTolenk <carlos.tolentinoe@gmail.com>
2022-10-10 08:56:56 -0400

    Add posted raw

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ScheduleModule } from '@nestjs/schedule';
 import { TasksService } from './task.service';
 import { ScrapperModule } from './scrapper/scrapper.module';
 
 @Module({
   imports: [ScheduleModule.forRoot(), ScrapperModule],
-  controllers: [AppController],
-  providers: [AppService, TasksService],
+  providers: [TasksService],
 })
 export class AppModule {}
